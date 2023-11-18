9b5d7730b7c50d2b590e770689d930d7835ea4d3
larissagoularte <58703854+larissagoularte@users.noreply.github.com>
2023-11-18 21:31:10 +0000

    Game finished. Refactor needed

--- game.js
+++ game.js
@@ -2,16 +2,22 @@ let playerChoice;
 let computerChoice;
 let playerScore = 0;
 let computerScore = 0;
-let choice;
 
+let choice;
+let winner;
+
+const resetBtn = document.getElementById('resetBtn')
 const player = document.querySelector('#pScore');
-player.textContent = `Player Score: ${playerScore}`
+
+
+resetBtn.addEventListener('click', resetGame)
+
+player.textContent = `player score: ${playerScore}`
 
 const computer = document.querySelector('#cScore');
-computer.textContent = `Computer Score: ${computerScore}`
+computer.textContent = `computer score: ${computerScore}`
 
 const output = document.querySelector('.output');
-output.textContent = "First player to 5 wins."
 
 const buttons = document.querySelectorAll(".btn");
 
@@ -19,9 +25,9 @@ buttons.forEach((button)=>{
     button.addEventListener('click', ()=>{
         playerChoice = button.id;
         computerChoice = (computerSelection())
-        console.log(playerScore)
-        play();
+        playRound();
         updateScores();
+        updateChoices(playerChoice, computerChoice);
     })
 })
 
@@ -39,43 +45,106 @@ function computerSelection(){
     return choice;
 }
 
-function play(){
-    if(playerScore===5){
-        output.textContent = "You win :)"
-        playerScore = 0;
-        computerScore = 0;
-    }else if(computerScore===5){
-        output.textContent = "You lose :("
-        playerScore = 0;
-        computerScore = 0;
+function playRound(){
+    if(
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ){
+        playerScore++
+        winner = 'player'
+    } else if(
+        (playerChoice === "rock" && computerChoice === "paper") ||
+        (playerChoice === "paper" && computerChoice === "scissors") ||
+        (playerChoice === "scissors" && computerChoice === "rock")
+    ){
+        computerScore++
+        winner = 'computer'
     } else{
-        if(
-            (playerChoice === "rock" && computerChoice === "scissors") ||
-            (playerChoice === "paper" && computerChoice === "rock") ||
-            (playerChoice === "scissors" && computerChoice === "paper")
-        ){
-            playerScore++
-            console.log('p: ' + playerChoice)
-            console.log('c: ' + computerChoice)
-        } else if(
-            (playerChoice === "rock" && computerChoice === "paper") ||
-            (playerChoice === "paper" && computerChoice === "scissors") ||
-            (playerChoice === "scissors" && computerChoice === "rock")
-        ){
-            computerScore++
-            console.log('p: ' + playerChoice)
-            console.log('c: ' + computerChoice)
-        } else{
-    
-            console.log('tie?')
-            console.log('p: ' + playerChoice)
-            console.log('c: ' + computerChoice)
-        }
+        winner = 'tie'
     }
+            
+    updateOutput(winner,playerChoice,computerChoice)
     
 }
 
 function updateScores() {
-    player.textContent = `Player Score: ${playerScore}`;
-    computer.textContent = `Computer Score: ${computerScore}`;
+    player.textContent = `player score: ${playerScore}`;
+    computer.textContent = `computer score: ${computerScore}`;
+}
+
+function updateChoices(playerChoice, computerChoice){
+    const pChoiceImg = document.getElementById('pChoiceImg')
+    const cChoiceImg = document.getElementById('cChoiceImg')
+
+    switch(playerChoice){
+        case "rock":
+            pChoiceImg.src = 'images/rock.png'
+            break
+        case "paper":
+            pChoiceImg.src = 'images/paper.png'
+            break
+        case "scissors":
+            pChoiceImg.src = 'images/scissors.png'
+            break
+    }
+
+    switch(computerChoice){
+        case "rock":
+            cChoiceImg.src = 'images/rock.png'
+            break
+        case "paper":
+            cChoiceImg.src = 'images/paper.png'
+            break
+        case "scissors":
+            cChoiceImg.src = 'images/scissors.png'
+            break
+    }
+}
+
+function updateOutput(winner, playerChoice, computerChoice){
+    if(playerScore===5){
+        output.textContent = "you won :D"
+        showResetBtn();
+    } else if(computerScore===5){
+        output.textContent = "you lost :("
+        showResetBtn();
+    } else {
+        if(winner === 'player'){
+            output.textContent = `you win! ${playerChoice} beats ${computerChoice}`
+        } else if(winner==='computer'){
+            output.textContent = `you lose! ${computerChoice} beats ${playerChoice}`
+        } else {
+            output.textContent = `it's a tie. ${computerChoice} ties with ${playerChoice}`
+        }
+    }   
+}
+
+function resetGame(){
+    hideResetBtn();
+    const pChoiceImg = document.getElementById('pChoiceImg')
+    const cChoiceImg = document.getElementById('cChoiceImg')
+
+    output.textContent = 'first to score 5 points wins!'
+    playerScore = 0;
+    computerScore = 0;
+    winner = "";
+    pChoiceImg.src = 'images/q-mark.png'
+    cChoiceImg.src = 'images/q-mark.png'
+
+    updateScores();
+}
+
+function showResetBtn(){
+    resetBtn.style.display = 'block';
+    buttons.forEach((button)=>{
+        button.classList.add("disabled-button")
+    })
+}
+
+function hideResetBtn() {
+    resetBtn.style.display = 'none';
+    buttons.forEach((button)=>{
+        button.classList.remove("disabled-button")
+    })
 }
